2a3c22bb5418eb14e2186c6f15c43ea1f5da15f9
Viktor Eikman <viktor.eikman@gmail.com>
2021-07-01 19:07:05 +0200

    Back to development.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject scad-klupe "0.2.0"
+(defproject scad-klupe "0.3.0-SNAPSHOT"
   :description "Threaded fasteners for scad-clj"
   :url "https://github.com/veikman/scad-klupe"
   :license {:name "Eclipse Public License"
